24ca5ac65dc03d48a48d216aa63e99a5119b857f
Andreas Geidnert <geidnert@gmail.com>
2015-12-17 08:35:09 +0100

    Google Cloud Messaging framework added

--- app/src/main/java/com/solidparts/gifts/LoginActivity.java
+++ app/src/main/java/com/solidparts/gifts/LoginActivity.java
@@ -3,20 +3,26 @@ package com.solidparts.gifts;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -27,7 +33,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.solidparts.gifts.dto.UserDTO;
+import com.solidparts.gifts.gcm.QuickstartPreferences;
+import com.solidparts.gifts.gcm.RegistrationIntentService;
 import com.solidparts.gifts.service.UserService;
 
 import java.util.ArrayList;
@@ -40,6 +50,10 @@ import java.util.List;
 public class LoginActivity extends ActionBarActivity implements LoaderCallbacks<Cursor> {
     public final static String EXTRA_EMAIL = "email";
     public final static String EXTRA_USERDTO = "userDTO";
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "LoginActivity";
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
 
     /**
      * A dummy authentication store containing known user names and passwords.
@@ -71,6 +85,41 @@ public class LoginActivity extends ActionBarActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // GCM
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+//                findViewById(R.id.progressBar).setVisibility(View.GONE);
+
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+                if (sentToken) {
+                    messageManager.show(getApplicationContext(), "Token retrieved and sent to server! " +
+                            "You can now use gcmsender to send downstream messages to this app", false);
+                } else {
+                    messageManager.show(getApplicationContext(), "An error occurred while either " +
+                            "fetching the InstanceID token, sending the fetched token to the server " +
+                            "or subscribing to the PubSub topic. Please try running the sample again.", false);
+                }
+            }
+        };
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+
+        // Check network status
+        messageManager = new MessageManager();
+        if (!userService.isNetworkAvaliable(this)) {
+            messageManager.show(this, "No network connection available!", true);
+            Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
         userService = new UserService(this);
         messageManager = new MessageManager();
 
@@ -100,6 +149,19 @@ public class LoginActivity extends ActionBarActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
     }
 
     private void populateAutoComplete() {
@@ -339,6 +401,27 @@ public class LoginActivity extends ActionBarActivity implements LoaderCallbacks<
             mAuthTask = null;
             showProgress(false);
         }
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
 
